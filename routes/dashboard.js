fb0b2b6bece6895c5bddd1584b4dbb296368ff8b
Vladimir Grinkevich <84560225+VladimirGrinkevich@users.noreply.github.com>
2023-04-17 10:38:17 +1200

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/", function(rq, rs, next){   // The name of the variables "rq" and "rs" should be meaningful. Add the description to the method.
-  rq.session.destroy();                 
+router.get("/", function(rq, rs, next){   // The name of the variables "rq" and "rs" should be meaningful 
+  rq.session.destroy();                   // Add the description to the method
   rs.redirect("/");
   });
 

7f4936d55a7e9391a2e882d39d4425e2e4a75282
Vladimir Grinkevich <84560225+VladimirGrinkevich@users.noreply.github.com>
2023-04-17 10:31:58 +1200

    Update unit.test.js

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -4,17 +4,17 @@ const req = request(baseURL);
 
 describe("Unit tests", () => {
 
-    it("Main page", async () => {                   // Add the description for the unit test
+    it("Main page", async () => {                   // Add the description of the unit test
         const res = await req.get("/");
         expect(res.statusCode).toBe(200);
     });
 
-    it("Login form", async () => {                  // Add the description for the unit test
+    it("Login form", async () => {                  // Add the description of the unit test
         const res = await req.get("/login");
         expect(res.statusCode).toBe(200);
     });
 
-    it("Register form", async () => {               // Add the description for the unit test
+    it("Register form", async () => {               // Add the description of the unit test
         const res = await req.get("/register");
         expect(res.statusCode).toBe(200);
     });

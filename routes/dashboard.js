a79029d1fd7bca740b6e67f005aa408a115ecaae
Vladimir Grinkevich <84560225+VladimirGrinkevich@users.noreply.github.com>
2023-04-16 23:01:24 +1200

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/", function(rq, rs, next){
-  rq.session.destroy();
-  rs.redirect("/");
+router.get("/", function(request, response, next){
+  request.session.destroy();
+  response.redirect("/");
   });
 
 module.exports = router;

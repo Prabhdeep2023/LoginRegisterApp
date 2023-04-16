bdba284795ae68eeac604786a4b25a773c232199
Vladimir Grinkevich <84560225+VladimirGrinkevich@users.noreply.github.com>
2023-04-17 10:34:32 +1200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var db = require('../database');
 
-router.get("/", (req, res) => {
+router.get("/", (req, res) => {                           // Add the description to the method
   var sessionvar = req.session;
   if (sessionvar.user_id)
       res.render("dashboard", { session: req.session });

2047cf2bde8a33196d3de1ab65881759b5763d38
Prabhdeep Singh <prsi744@ess.ais.ac.nz>
2023-04-18 19:03:21 +1200

    first commit

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -1,21 +1,24 @@
 const request = require("supertest");
 const baseURL = "http://localhost:7123"
-const req = request(baseURL);
+const request = request(baseURL);
 
 describe("Unit tests", () => {
 
+    /* test for checking if the main page loads successfully */
     it("Main page", async () => {
-        const res = await req.get("/");
-        expect(res.statusCode).toBe(200);
+        const response = await request.get("/");
+        expect(response.statusCode).toBe(200);
     });
 
+    /* test for checking if the login page loads successfully */
     it("Login form", async () => {
-        const res = await req.get("/login");
-        expect(res.statusCode).toBe(200);
+        const response = await request.get("/login");
+        expect(response.statusCode).toBe(200);
     });
 
+    /* test for checking if the register page loads successfully */
     it("Register form", async () => {
-        const res = await req.get("/register");
-        expect(res.statusCode).toBe(200);
+        const response = await request.get("/register");
+        expect(response.statusCode).toBe(200);
     });
 });
